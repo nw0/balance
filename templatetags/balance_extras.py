5a1b4a53bc8ef3932bf3506c06e0a97543a63122
Nicholas Sim <nicholassimws@gmail.com>
2016-12-30 23:15:37 +0000

    view: EntryMonth: show entries in table

--- templatetags/balance_extras.py
+++ templatetags/balance_extras.py
@@ -1,6 +1,9 @@
 import datetime
+import numbers
 
 from django import template
+from django.utils.safestring import mark_safe
+from moneyed import Money
 
 register = template.Library()
 
@@ -13,6 +16,19 @@ def months(start, times):
         yield date
         it -= 1
         date -= datetime.timedelta(days=1)
+
+
+@register.filter(is_safe=True)
+def accountform(value):
+    currency = ""
+    if isinstance(value, Money):
+        currency = " %s" % value.currency
+        value = value.amount
+    if not isinstance(value, (numbers.Number, Money)):
+        return ""
+    if value > 0:
+        return mark_safe("%s%s" % (value, currency))
+    return mark_safe('<span class="text-danger">(%s%s)</span>' % (-value, currency))
 
 
 @register.inclusion_tag('balance/embeds/cat_tr.html')
