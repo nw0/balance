0d4ce5a380acb63b5588bea527b9abfddcb3dca7
Nicholas Sim <nicholassimws@gmail.com>
2017-12-16 18:46:22 +0000

    views: CategoryCreate: add

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ app_name = 'balance'
 urlpatterns = [
     path('', views.AccountList.as_view(), name='account_list'),
     path('account/create/', views.AccountCreate.as_view(), name='account_create'),
+    path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
 ]
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .models import Account
+from .models import Account, TransactionCategory
 
 
 class AccountList(generic.ListView):
@@ -17,3 +17,13 @@ class AccountCreate(generic.edit.CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super(AccountCreate, self).form_valid(form)
+
+
+class CategoryCreate(generic.edit.CreateView):
+    model = TransactionCategory
+    fields = ['name']
+    success_url = reverse_lazy('balance:account_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(CategoryCreate, self).form_valid(form)
