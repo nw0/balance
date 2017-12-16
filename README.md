73eda2a4d1f13414dbe169ce44ef4c9c497b739e
Nicholas Sim <nicholassimws@gmail.com>
2017-12-16 15:28:11 +0000

    fresh app for rewrite

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 basic expense manager
 
 ## Requirements
-1. [`django-bootstrap3`](https://github.com/dyve/django-bootstrap3) for forms
+1. [`django-bootstrap4`](https://github.com/zostera/django-bootstrap4) for forms
 1. [`django-money`](https://github.com/django-money/django-money/)
--- models.py
+++ models.py
@@ -1,36 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
-from djmoney.models.fields import MoneyField
-
-
-@python_2_unicode_compatible
-class Category(models.Model):
-    name = models.CharField(max_length=30)
-    owner = models.ForeignKey(User)
-
-    def monthly(self, date):
-        sum = Entry.objects.filter(category=self, date__year=date.year, date__month=date.month).aggregate(
-            models.Sum('amount'))['amount__sum']
-        return sum or 0
-
-    class Meta:
-        verbose_name_plural = "categories"
-
-    def __str__(self):
-        return self.name
-
-
-@python_2_unicode_compatible
-class Entry(models.Model):
-    amount = MoneyField(max_digits=10, decimal_places=2, default_currency="GBP")
-    date = models.DateField()
-    note = models.CharField(max_length=50)
-    category = models.ForeignKey(Category)
-
-    class Meta:
-        verbose_name_plural = "entries"
-
-    def __str__(self):
-        return "%s: [%s] %s %s" % (self.date, self.category, self.note, self.amount)
+# Create your models here.
