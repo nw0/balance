e365182386859c755cde6f3a033dcd442a82cbd2
Nicholas Sim <nicholassimws@gmail.com>
2017-12-16 18:34:27 +0000

    views: AccountCreate: add

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'balance'
 urlpatterns = [
     path('', views.AccountList.as_view(), name='account_list'),
+    path('account/create/', views.AccountCreate.as_view(), name='account_create'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Account
@@ -6,3 +7,13 @@ from .models import Account
 class AccountList(generic.ListView):
     def get_queryset(self):
         return Account.objects.filter(owner=self.request.user)
+
+
+class AccountCreate(generic.edit.CreateView):
+    model = Account
+    fields = ['name', 'owned']
+    success_url = reverse_lazy('balance:account_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(AccountCreate, self).form_valid(form)
