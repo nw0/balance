aaec533acf64ec978f4b7e5b983d83a6a102e981
Nicholas Sim <nicholassimws@gmail.com>
2017-12-16 16:00:29 +0000

    models: TransactionCategory, Transaction: add

--- models.py
+++ models.py
@@ -23,3 +23,28 @@ class AccountBalance(models.Model):
 
     class Meta:
         verbose_name = "balance record"
+
+
+class TransactionCategory(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+
+class Transaction(models.Model):
+    owner = models.ForeignKey(User)
+    payee = models.ForeignKey(Account, blank=True)
+    payer = models.ForeignKey(Account, blank=True)
+    date = models.DateField()
+    category = models.ForeignKey(TransactionCategory)
+    remark = models.CharField(max_length=50)
+    amount = MoneyField(max_digits=16, decimal_places=2, default_currency="GBP")
+
+    def __str__(self):
+        return "%s (%s: %s)" % (self.amount, self.category, self.remark)
